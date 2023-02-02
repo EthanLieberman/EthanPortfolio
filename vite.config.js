cb90de3ec2ecdc2f51a85e9a67ec0e269ad9338b
Ethan Lieberman <ethan.lieberman@yahoo.com>
2023-02-01 18:07:03 -0700

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/docs/',
+  base: '/EthanPortfolio/docs/',
   plugins: [vue()],
   resolve: {
     alias: {
